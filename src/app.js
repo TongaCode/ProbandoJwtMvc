c0297ad039bb6f185b822bb80269f9c76650a7ce
Gaston Iaria <iariagaston@gmail.com>
2024-08-08 18:44:54 -0300

    Probando jwt en mvc

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
 
+// Cargar las variables de entorno desde el archivo .env
+require('dotenv').config();
+
+//Variables de entorno
+const port = process.env.port;
+
 //Middelwares
 app.use(express.json());
 
 
-app.listen();
+app.listen(port, ()=> {
+    console.log('Server Online.');
+});
