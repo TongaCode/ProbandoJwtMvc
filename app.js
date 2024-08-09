1f08ccc3e444ad4a1e3a8c122588a33a067e2d5b
Gaston Iaria <iariagaston@gmail.com>
2024-08-09 02:14:28 -0300

    Probando jwt en mvc

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const app = express();
+const  router  = require('./src/routers/index.router');
 
 // Cargar las variables de entorno desde el archivo .env
 require('dotenv').config();
 
 // Variables de entorno
-const port = process.env.port;
+const port = process.env.PORT;
 
 // Middelwares
 app.use(express.json());
 
+// Routers
+app.use(router);
 
-app.listen(port, ()=> {
+
+app.listen(port, () => {
     console.log('Server Online.');
 });
---
+++ src/controllers/index.controller.js
@@ -0,0 +1,6 @@
+const controller = {};
+
+controller.index = (req, res) => { 
+    res.send('hola desde el index.router')};
+
+    module.exports = controller;
