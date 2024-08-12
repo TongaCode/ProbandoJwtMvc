4858c68c6066ce47eac65850f0534950f287a19b
Gaston Iaria <iariagaston@gmail.com>
2024-08-12 04:59:32 -0300

    Probando jwt en mvc

---
+++ confing/mongo.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Conectado a la base de datos mongo db!.');
+  } catch (err) {
+    console.error('Error al conectar a la base de datos mongo:', err.message);
+    process.exit(1); // Termina el proceso en caso de error de conexión
+  }
+};
+
+module.exports = connectDB;
