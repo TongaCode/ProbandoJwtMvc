b6ea7f308df7830241c46f1ba110a57467973f70
Gaston Iaria <iariagaston@gmail.com>
2024-11-28 13:42:23 -0300

    Agrege validacion en el login

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -37,51 +37,48 @@ controller.buscarUsuario = async (req, res) => {
         const usuario = await usuarioModel.findOne({ email: email });
 
         if (usuario === null) {
-            return res.status(200).json('El usuario no existe!')
+            return res.status(400).json('El usuario no existe!')
         }
 
         return res.status(200).json({usuario});
 
         
     } catch (error) {
-        return res.status(400).json({ error: error.message });
+        return res.status(500).json({ error: error.message });
     }
 
 };
 
 controller.login = async (req, res) => {
-
     try {
-        const { usuario, password } = req.body;
 
-        const user = await usuarioModel.findOne({ usuario: usuario }).select('usuario password email');
-
+        // Si el usuario no existe
         if (!user) {
-            throw new Error('El usuario no existe!.');
-        };
+            errores.push('Credenciales incorrectas' );
+        }
+        // Buscar al usuario en la base de datos
+        const user = await usuarioModel.findOne({ usuario }).select('usuario password email');
 
-        if (user.password !== password) {
-            throw new Error('La contraseña es invalida!.');
-        };
-
-        //Genero el payload con el mail del usuario.
+        // Generar el payload con el email del usuario
         const payload = { email: user.email };
+
+        // Generar el token JWT
         const token = generateToken(payload);
 
         // Enviar el token JWT como una cookie
         res.cookie('token', token, {
             httpOnly: true,
-            secure: false, // Cambia a true si usas HTTPS en producción
+            secure: process.env.NODE_ENV === 'production', // Habilitar secure en producción
             maxAge: 24 * 60 * 60 * 1000 // 1 día
         });
 
+        // Responder con éxito
+        return res.status(200).json({ message: 'Bienvenido' });
 
-
-        res.status(200).json({ message: 'bienvenido' })
     } catch (error) {
-        return res.status(400).json({ error: error.message });
-    }
 
+        return res.status(500).json({ error: 'Error en el servidor' });
+    }
 };
 
 module.exports = controller;
