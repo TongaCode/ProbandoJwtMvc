249cbfae7c41de25a63c444d2bc4cb8956da6b97
Gaston Iaria <iariagaston@gmail.com>
2024-11-29 02:42:36 -0300

    Middleware para verificar si el email existe en la base de datos

---
+++ src/middelware/verificarEmail.js
@@ -0,0 +1,16 @@
+const usuarioModel = require("../models/usuarios");
+
+async function verificarEmail(req, res, next) {
+
+    const { email } = req.body;
+    const verificar = await usuarioModel.findOne({ email: email });
+
+    if (verificar) {
+        return res.status(400).json('El email ya existe!.');
+    }
+
+    next();
+
+};
+
+module.exports = verificarEmail;
