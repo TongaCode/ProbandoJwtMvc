7820a7ba81e416ed9bb515a3e6d4093ff6668b55
Gaston Iaria <iariagaston@gmail.com>
2024-11-29 04:59:08 -0300

    Funcion para generar el JWT4

---
+++ src/middelware/validarToken.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+
+function validarToken(req, res, next) {
+    
+    try {
+        // Obtener el token desde las cookies, los encabezados o el cuerpo de la solicitud
+        const token = req.cookies.token;
+    
+        // Si no hay token, devolver un error
+        if (!token) {
+            return res.status(401).json({ error: 'Acceso denegado. Token no proporcionado.' });
+        }
+        // Verificar el token
+        const decoded = jwt.verify(token, process.env.SECRET);
+
+        // Adjuntar la información decodificada al objeto `req` para su uso posterior
+        req.user = decoded;
+
+        // Pasar al siguiente middleware o controlador
+        //return res.status(200).json(req.user) solo para prueba
+        next();
+        
+    } catch (error) {
+        return res.status(401).json({ error: 'Token inválido o expirado.' });
+    }
+}
+
+module.exports = validarToken;
