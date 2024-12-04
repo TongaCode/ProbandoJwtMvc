4265e8be3276a16f05820c464ba61b0a130d6725
Gaston Iaria <iariagaston@gmail.com>
2024-12-04 04:13:40 -0300

    Elimine validar fondos del repository ya que lo modifique por una middelware reutilizable

--- src/service/validarFondosService.js
+++
@@ -1,9 +0,0 @@
-const validarFondos = {};
-
-validarFondos.service = (emisor, moneda, monto) => {
-    if (emisor[moneda] < monto) throw new Error('No tienes fondos suficientes!.');
-    
-    return true;
-};
-
-module.exports = validarFondos;
